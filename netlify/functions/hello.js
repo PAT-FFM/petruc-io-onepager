638a0561589f28a88d66c1cfe7b53853232f085c
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2025-07-08 15:00:44 +0200

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,3 +1,15 @@
-export default () => new Response("Hello world");
+export default async (request: Request, context: Context) => {
+  const url = new URL(request.url);
+
+  // Look for the query parameter, and return if we don't find it
+  if (url.searchParams.get("method") !== "transform") {
+    return;
+  }
+
+  const response = await context.next();
+  const text = await response.text();
+
+  return new Response(text.toUpperCase(), response);
+}
 
 export const config = { path: "/test" };

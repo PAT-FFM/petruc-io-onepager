8c736b007e7af01d7dc0f3d06e55c5224d5b7ee0
PAT-FFM <118531774+PAT-FFM@users.noreply.github.com>
2025-07-08 15:47:11 +0200

    Update hello.js

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,5 +1,5 @@
 export default async (req, context) => {
-  console.log(context.geo.city + " / " + context.ip);
+  console.log(context.geo.city + " / " + context.ip + " / " + req.headers.get("User-Agent"));
 };
 
 export const config = { path: "/hello" };
